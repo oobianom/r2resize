3d43356791d1d8992ab1b39b4476e6a52da781af
Obi Obianom <oobianom@umaryland.edu>
2022-10-12 03:31:59 -0400

    update to new feature
    resizable div

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,12 @@
+# r2resize 1.3
+
+## Features
+
+  [x] Resizable split screen container
+
 # r2resize 1.2
 
 ## Features
 
-* Image and table resizing toolbar
-* User enabled font-family and font-size change
+  [x] Image and table resizing toolbar
+  [x] User enabled font-family and font-size change
--- R/containerResizer.R
+++ R/containerResizer.R
@@ -8,8 +8,8 @@
 #' @param width.right color of the page font in e.g. darkblue or #006699
 #' @param width.units boolean. TRUE or FALSE, to add resize toobar to tables
 #' @param splitter.color splitter color e.g red, black or #333333
-#' @param left.color left panel color e.g red, black or #333333
-#' @param right.color right panel color e.g red, black or #333333
+#' @param bg.left.color left panel color e.g red, black or #333333
+#' @param bg.right.color right panel color e.g red, black or #333333
 #' @param left.bg.url left panel background image e.g image1.png or https://..image1.png
 #' @param right.bg.url right panel background image e.g image1.png or https://..image1.png
 #' @param border color of the resizer track e.g. red or #f5f5f5
@@ -23,29 +23,30 @@
 #' @return Split screen container
 #'
 #' @examples
-#' r2resize::add.resizer() #default settings
 #'
-#' r2resize::add.resizer(
-#'   theme.color = "blue",
-#'   position = "top",
-#'   font.size = "12px",
-#'   font.color = "brown",
-#'   tables = TRUE,
-#'   images = TRUE,
-#'   line.color = "green",
-#'   line.width = 150,
-#'   line.height = 5
-#' ) # customized settings
+#' r2resize::splitCard(
+#'   "sample rpkg.net 1",
+#'   "sample rpkg.net 2 ",
+#'   splitter.color = "transparent",
+#'   bg.right.color = "pink",
+#'   bg.left.color = "gray",
+#'   text.left.color = "green",
+#'   text.right.color = "yellow",
+#'   position = "vertical"
+#' )
 #'
 #' @export
 
-splitCard <- function(left,right,width.left = 50, width.right = 50,
-                      width.units = "%", splitter.color = "gray", left.color = "yellow",
+splitCard <- function(left, right,
+                      width.left = 50,
+                      width.right = 50,
+                      width.units = "%", splitter.color = "gray",
+                      bg.left.color = "yellow",
                       left.bg.url = NULL,
                       right.bg.url = NULL,
-                      right.color = "lightgray",
-                      border = c("none","primary","secondary","warning","danger","success"),
-                      height = NULL, position= c("vertical","horizontal"),
+                      bg.right.color = "lightgray",
+                      border = c("none", "primary", "secondary", "warning", "danger", "success"),
+                      position = c("vertical", "horizontal"),
                       text.left.color = "white",
                       text.right.color = "black") {
 
@@ -55,35 +56,34 @@ splitCard <- function(left,right,width.left = 50, width.right = 50,
   # fetch selected border
   border <- match.arg(border)
 
-  #set splitter classes
+  # set splitter classes
 
-  switch (position,
+  switch(position,
     vertical = {
       class.0 <- "container"
       class.a <- "left"
       class.b <- "splitter"
       class.c <- "right"
-      class.d <- 'Height'
+      class.d <- "Height"
     },
     horizontal = {
       class.0 <- "container-horizontal"
       class.a <- "top"
       class.b <- "splitter-horizontal"
       class.c <- "bottom"
-      class.d <- 'Width'
+      class.d <- "Width"
     }
   )
 
-  holders <- paste0("r2resize-resizablediv-panel-",class.0)
-    panel.a <- paste0("r2resize-resizablediv-panel-",class.a)
-    splitters <- paste0("r2resize-resizablediv-",class.b)
-    panel.b <- paste0("r2resize-resizablediv-panel-",class.c)
+  holders <- paste0("r2resize-resizablediv-panel-", class.0)
+  panel.a <- paste0("r2resize-resizablediv-panel-", class.a)
+  splitters <- paste0("r2resize-resizablediv-", class.b)
+  panel.b <- paste0("r2resize-resizablediv-panel-", class.c)
   # script fetch js
   theme.02.js <- paste0(template.loc(), "/divsplitter.js")
   script <- ""
   if (file.exists(theme.02.js)) {
-    print("exists---")
-    script <- paste(c("<script>", readLines(theme.02.js), "</script>"),collapse = " ")
+    script <- paste(c("<script>", readLines(theme.02.js), "</script>"), collapse = " ")
     script <- gsub("resizepanelwhich", panel.a, script)
     script <- gsub("resizesplitterwhich", splitters, script)
     script <- gsub("HeWigdht", class.d, script)
@@ -93,32 +93,31 @@ splitCard <- function(left,right,width.left = 50, width.right = 50,
   class(script) <- c("html", "character")
 
   # set initial content
-  bgcol <-"background-color:"
-  bgurl <-"background-image:url("
-  textcol <-"color:"
+  bgcol <- "background-color:"
+  bgurl <- "background-image:url("
+  textcol <- "color:"
   shiny::div(
     shiny::div(
       class = holders,
       shiny::div(
         class = panel.a,
-        style = paste0(bgcol,left.color),
-        style = ifelse(is.null(left.bg.url),"",paste0(bgurl,left.bg.url,")")),
-        style = ifelse(is.null(text.left.color),"",paste0(textcol,text.left.color,"")),
+        style = paste0(bgcol, bg.left.color, ";"),
+        style = ifelse(is.null(left.bg.url), "", paste0(bgurl, left.bg.url, ");")),
+        style = ifelse(is.null(text.left.color), "", paste0(textcol, text.left.color, ";")),
         left
       ),
       shiny::div(
         class = splitters,
-        style = paste0(bgcol,splitter.color)
+        style = paste0(bgcol, splitter.color)
       ),
       shiny::div(
         class = panel.b,
-        style = paste0(bgcol,right.color),
-        style = ifelse(is.null(right.bg.url),"",paste0(bgurl,right.bg.url,")")),
-        style = ifelse(is.null(text.right.color),"",paste0(textcol,text.right.color,"")),
+        style = paste0(bgcol, bg.right.color, ";"),
+        style = ifelse(is.null(right.bg.url), "", paste0(bgurl, right.bg.url, ");")),
+        style = ifelse(is.null(text.right.color), "", paste0(textcol, text.right.color, ";")),
         right
       )
     ),
     script
   )
-
 }

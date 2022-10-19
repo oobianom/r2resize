cd62288a656987ec736c7e94b5a66c962350398d
Obi Obianom <oobianom@umaryland.edu>
2022-10-19 01:59:39 -0400

    rezcontCard
    update

--- inst/themes/rezcontCard.js
+++ inst/themes/rezcontCard.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   $(".r2resize-rezconcard-controls-small").click(function(event){
     event.preventDefault();
-    $(".r2resize-rezconcard-controls").animate({"font-size":"12px", "line-height":"16px"});
+    $(".r2resize-rezconcard-holder").animate({"font-size":"50%", "line-height":"16px"});
     $("h1").animate({"font-size":"24px"});
     $("h2").animate({"font-size":"16px"});
 
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
   $(".r2resize-rezconcard-controls-medium").click(function(event){
     event.preventDefault();
-    $(".r2resize-rezconcard-controls").animate({"font-size":"14px", "line-height":"20px"});
+    $(".r2resize-rezconcard-holder").animate({"font-size":"100%", "line-height":"20px"});
     $("h1").animate({"font-size":"36px"});
     $("h2").animate({"font-size":"24px"});
 
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
   $(".r2resize-rezconcard-controls-large").click(function(event){
     event.preventDefault();
-    $(".r2resize-rezconcard-controls").animate({"font-size":"16px", "line-height":"20px"});
+    $(".r2resize-rezconcard-holder").animate({"font-size":"200%", "line-height":"20px"});
     $("h1").animate({"font-size":"48px"});
     $("h2").animate({"font-size":"30px"});
 

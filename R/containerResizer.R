f50ac73c6c7a67c784614c145b9196d8a7a3f75e
Obi Obianom <oobianom@umaryland.edu>
2022-10-19 01:40:46 -0400

    adding the rezcontcard function
    rezcont

--- R/containerResizer.R
+++ R/containerResizer.R
@@ -2,17 +2,17 @@
 #'
 #' Highly customizable and resizable split screen container
 #'
-#' @param left theme color for resizer and table e.g. black or #000000
-#' @param right position of the resize toolbar e.g. top or bottom
-#' @param width.left size of the page font in px e.g. 14px
-#' @param width.right color of the page font in e.g. darkblue or #006699
+#' @param left content on the left or top
+#' @param right content on the right or bottom
+#' @param width.left width of the left or top
+#' @param width.right width of the right or bottom
 #' @param width.units boolean. TRUE or FALSE, to add resize toobar to tables
 #' @param splitter.color splitter color e.g red, black or #333333
 #' @param bg.left.color left panel color e.g red, black or #333333
 #' @param bg.right.color right panel color e.g red, black or #333333
 #' @param left.bg.url left panel background image e.g image1.png or https://..image1.png
 #' @param right.bg.url right panel background image e.g image1.png or https://..image1.png
-#' @param border color of the resizer track e.g. red or #f5f5f5
+#' @param border.color color of the container e.g. red or #f5f5f5
 #' @param text.left.color color of left panel text
 #' @param text.right.color color of right panel text
 #'
@@ -45,7 +45,7 @@ splitCard <- function(left, right,
                       left.bg.url = NULL,
                       right.bg.url = NULL,
                       bg.right.color = "lightgray",
-                      border = c("none", "primary", "secondary", "warning", "danger", "success"),
+                      border.color = NULL,
                       position = c("vertical", "horizontal"),
                       text.left.color = "white",
                       text.right.color = "black") {
@@ -54,7 +54,7 @@ splitCard <- function(left, right,
   position <- match.arg(position)
 
   # fetch selected border
-  border <- match.arg(border)
+  border <- border.color
 
   # set splitter classes
 
@@ -120,4 +120,91 @@ splitCard <- function(left, right,
     ),
     script
   )
+}
+
+
+
+#' Resizable container content holder
+#'
+#' Highly customizable and resizable  container content holder
+#'
+#' @param ... content
+#' @param width.left size of the page font in px e.g. 14px
+#' @param width.right color of the page font in e.g. darkblue or #006699
+#' @param width.units boolean. TRUE or FALSE, to add resize toobar to tables
+#' @param splitter.color splitter color e.g red, black or #333333
+#' @param bg.left.color left panel color e.g red, black or #333333
+#' @param bg.right.color right panel color e.g red, black or #333333
+#' @param left.bg.url left panel background image e.g image1.png or https://..image1.png
+#' @param right.bg.url right panel background image e.g image1.png or https://..image1.png
+#' @param border.color color of the container e.g. red or #f5f5f5
+#' @param text.left.color color of left panel text
+#' @param text.right.color color of right panel text
+#'
+#' @section Examples for r2resize:
+#' More examples and demo pages for are located at this link -
+#' \url{https://rpkg.net/package/r2resize}.
+#'
+#' @return Split screen container
+#'
+#' @examples
+#'
+#' r2resize::splitCard(
+#'   "sample rpkg.net 1",
+#'   "sample rpkg.net 2 ",
+#'   splitter.color = "transparent",
+#'   bg.right.color = "pink",
+#'   bg.left.color = "gray",
+#'   text.left.color = "green",
+#'   text.right.color = "yellow",
+#'   position = "vertical"
+#' )
+#'
+#' @export
+
+rezcontCard <- function(...,
+                      width.left = 50,
+                      width.right = 50,
+                      width.units = "%", splitter.color = "gray",
+                      bg.left.color = "yellow",
+                      left.bg.url = NULL,
+                      right.bg.url = NULL,
+                      bg.right.color = "lightgray",
+                      border.color = NULL,
+                      position = c("vertical", "horizontal"),
+                      text.left.color = "white",
+                      text.right.color = "black") {
+
+  # fetch selected position
+  position <- match.arg(position)
+
+  # fetch selected border
+  border <- border.color
+
+  # script fetch js
+  theme.02.js <- paste0(template.loc(), "/rezcontCard.js")
+  script <- ""
+  if (file.exists(theme.02.js)) {
+    script <- paste(c("<script>", readLines(theme.02.js), "</script>"), collapse = " ")
+    # script <- gsub("resizepanelwhich", panel.a, script)
+    # script <- gsub("resizesplitterwhich", splitters, script)
+    # script <- gsub("HeWigdht", class.d, script)
+  }
+  # set to html
+  attr(script, "html") <- TRUE
+  class(script) <- c("html", "character")
+
+  #content
+  shiny::div(
+  shiny::div(
+    class = "r2resize-rezconcard-holder",
+    shiny::div(
+      class = "r2resize-rezconcard-controls",
+      shiny::tags$a(href = "#",class = "r2resize-rezconcard-controls-small","A"),
+      shiny::tags$a(href = "#",class = "r2resize-rezconcard-controls-medium","A"),
+      shiny::tags$a(href = "#",class = "r2resize-rezconcard-controls-large","A")
+    ),
+    ...
+  ),
+  script)
 }

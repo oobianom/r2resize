c61c923d25c3b13d0988f2b104e2b909db503db8
Obi Obianom <oobianom@umaryland.edu>
2023-04-06 03:04:36 -0400

    Update expandingAccordian.js

--- inst/themes/expandingAccordian.js
+++ inst/themes/expandingAccordian.js
@@ -1,6 +1,3 @@
 /*Expanding accordions*/
-$(".r2resize-flexCard-option").click(function(){
-   $(".r2resize-flexCard-option").removeClass("r2resize-flexCard-active");
-   $(this).addClass("r2resize-flexCard-active");
+$(".r2resize-flexCard-option").click(function(){$(".r2resize-flexCard-option").removeClass("r2resize-flexCard-active"),$(this).addClass("r2resize-flexCard-active")});
 
-});

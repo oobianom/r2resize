d45ebd902163bc1ebd31a6c386ce2093d3f437fb
Obi Obianom <oobianom@umaryland.edu>
2023-07-07 01:35:26 -0400

    Update markdownResizer.R

--- R/markdownResizer.R
+++ R/markdownResizer.R
@@ -83,7 +83,8 @@ add.resizer <- function(theme.color = NULL,
   if (file.exists(theme.02.js)) {
     con <- c(con, "<script>", readLines(theme.02.js), "</script>")
     con <- gsub("listgroupixon", "xxxxx", con)
-    if(!is.null(default.image.width)) con <- gsub("'pre3e2423'", "xxxxx", con)
+    if(!is.null(default.image.width))
+      con <- gsub("'pre3e2423'", paste0("'",default.image.width,"'"), con)
   }
 
   # font size

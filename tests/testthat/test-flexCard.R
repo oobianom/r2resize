376527bbe2e62cab36ec06aaf681f7e52ca18e34
Obi Obianom <oobianom@umaryland.edu>
2023-08-26 17:25:04 -0400

    Edit to tests for flexcard

--- tests/testthat/test-flexCard.R
+++ tests/testthat/test-flexCard.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Simple test for flexcard function", {
+  r2resize::flexCard(
+    item1 = c(
+    bg = "https://r2resize.obi.obianom.com/m/image1.jpg",
+    icon = "edit",
+    title="Sample text 1",
+    subtitle="A cool subtitle"),
+    item2 = c(
+    bg = "https://r2resize.obi.obianom.com/m/image2.jpg",
+    icon = "edit",
+    title="Sample text 2",
+    subtitle="A cool subtitle",
+    icon.color = "blue",
+    text.color = "brown"
+    )
+  )
 })

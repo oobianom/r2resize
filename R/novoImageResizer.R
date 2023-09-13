60232300dcc550abf7141fd83d7dd6f4d0b44efc
Obi Obianom <oobianom@umaryland.edu>
2023-09-13 01:07:41 -0400

    Add use case feature

--- R/novoImageResizer.R
+++ R/novoImageResizer.R
@@ -3,27 +3,29 @@
 #' Elegant image viewer and resizer for images
 #'
 #' @export
+#' @section Use case:
+#' Use in a shiny application for image(s) that you'd like to carry a viewer feature on click.\cr\cr
+#'    - Very portable
+#'
 #'
 
-shinyExpandImage <- function(imageid){
-  #declare folder name for htmltools
+shinyExpandImage <- function(imageid) {
+  # declare folder name for htmltools
   ssp <- "r2resize"
   vs <- "0.0.0"
-  quickcode::add_key(imageid) #add keys to the vector of ids
+  quickcode::add_key(imageid) # add keys to the vector of ids
 
-  #embed in a div and render
+  # embed in a div and render
   htmltools::tags$div(
     htmltools::htmlDependency(
-    ssp, vs,
-    src = file.path(find.package(package = ssp), "themes"),
-    stylesheet = "imgviewer.css",
-    script = "imgviewer.js",
-    all_files = FALSE
+      ssp, vs,
+      src = file.path(find.package(package = ssp), "themes"),
+      stylesheet = "imgviewer.css",
+      script = "imgviewer.js",
+      all_files = FALSE
     ),
-    lapply(imageid,function(i)
-    htmltools::tags$script(paste0('$(document).ready(function() { var $initScope',i$key,' = $("#',i$value,'"); if ($initScope',i$key,'.length) { $initScope',i$key,'.justifiedGallery( { border: -1, rowHeight: 150, margins: 8, waitThumbnailsLoad: true, randomize: false, }).on("jg.complete", function() { $initScope',i$key,'.lightGallery( { thumbnail: true, animateThumb: true, showThumbByDefault: true, }); }); }; $initScope',i$key,'.on("onAfterOpen.lg", function(event) { $("body").addClass("overflow-hidden"); }); $initScope',i$key,'.on("onCloseAfter.lg", function(event) { $("body").removeClass("overflow-hidden"); }); });'))
-    )
-    )
-
+    lapply(imageid, function(i) {
+      htmltools::tags$script(paste0("$(document).ready(function() { var $initScope", i$key, ' = $("#', i$value, '"); if ($initScope', i$key, ".length) { $initScope", i$key, '.justifiedGallery( { border: -1, rowHeight: 150, margins: 8, waitThumbnailsLoad: true, randomize: false, }).on("jg.complete", function() { $initScope', i$key, ".lightGallery( { thumbnail: true, animateThumb: true, showThumbByDefault: true, }); }); }; $initScope", i$key, '.on("onAfterOpen.lg", function(event) { $("body").addClass("overflow-hidden"); }); $initScope', i$key, '.on("onCloseAfter.lg", function(event) { $("body").removeClass("overflow-hidden"); }); });'))
+    })
+  )
 }
-

fa7d042b4e3d320b8620a22f300a52c9c9a80576
Obi Obianom <oobianom@umaryland.edu>
2023-09-13 00:20:29 -0400

    Update novoImageResizer.R

--- R/novoImageResizer.R
+++ R/novoImageResizer.R
@@ -1,15 +1,20 @@
+#' Add shiny resize for images
+#'
+#' Elegant image viewer and resizer for images
+#'
 #' @export
 #'
 
 addShinyImageResizer <- function(){
   ssp <- "r2resize"
   vs <- "0.0.0"
-  template.loc1 <- file.path(find.package(package = ssp), "scripts")
+  template.loc1 <- file.path(find.package(package = ssp), "themes")
   htmltools::tags$div(
     htmltools::htmlDependency(
     ssp, vs,
     src = template.loc1,
-    script = c("dexie.js", paste0(tolower(ssp), ".js")),
+    stylesheet = "imgviewer.css",
+    script = "imgviewer.js",
     all_files = FALSE
     )
   )

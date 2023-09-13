72a73e8e222d5f0e514aa91be53f16535bac1c9d
Obi Obianom <oobianom@umaryland.edu>
2023-09-13 00:17:49 -0400

    Update novoImageResizer.R

--- R/novoImageResizer.R
+++ R/novoImageResizer.R
@@ -2,6 +2,17 @@
 #'
 
 addShinyImageResizer <- function(){
+  ssp <- "r2resize"
+  vs <- "0.0.0"
+  template.loc1 <- file.path(find.package(package = ssp), "scripts")
+  htmltools::tags$div(
+    htmltools::htmlDependency(
+    ssp, vs,
+    src = template.loc1,
+    script = c("dexie.js", paste0(tolower(ssp), ".js")),
+    all_files = FALSE
+    )
+  )
 
 }
 

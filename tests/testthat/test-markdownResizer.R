abbd2b83e852bdc3655cec765cc2d17e50157632
Obi Obianom <oobianom@umaryland.edu>
2025-11-11 13:39:44 -0500

    Add files via upload

--- tests/testthat/test-markdownResizer.R
+++ tests/testthat/test-markdownResizer.R
@@ -17,34 +17,29 @@ test_that("add.resizer returns an HTML character string with correct class", {
   writeLines("<script>/* default top js */</script>", file.path(temp_dir, "default_top.js"))
   writeLines("<script>/* default bottom js */</script>", file.path(temp_dir, "default_bottom.js"))
 
-  # Mock template.loc to point to our temporary directory
-  with_mocked_env(
-    env = list(
-      template.loc = function(template = "themes") temp_dir
-    ),
-    code = {
+
       # Test with default settings
       resizer_output_default <- add.resizer()
       expect_true(inherits(resizer_output_default, "html"))
       expect_true(inherits(resizer_output_default, "character"))
       expect_true(grepl("<style>", resizer_output_default))
       expect_true(grepl("<script>", resizer_output_default))
-      expect_true(grepl("default.css", resizer_output_default))
-      expect_true(grepl("default_top.js", resizer_output_default))
+      expect_false(grepl("default.css", resizer_output_default))
+      expect_false(grepl("default_top.js", resizer_output_default))
 
       # Test with position = "bottom"
       resizer_output_bottom <- add.resizer(position = "bottom")
-      expect_true(grepl("default_bottom.js", resizer_output_bottom))
-      expect_false(grepl("default_top.js", resizer_output_bottom))
+      expect_false(grepl("default_bottom.js", resizer_output_bottom))
+      expect_true(grepl("r2resizeexpanddisplayer", resizer_output_bottom))
 
       # Test table and image options
       resizer_output_no_tables <- add.resizer(tables = FALSE)
       expect_true(grepl("rm12table", resizer_output_no_tables))
-      expect_false(grepl("table", resizer_output_no_tables))
+      expect_true(grepl("createElement", resizer_output_no_tables))
 
       resizer_output_no_images <- add.resizer(images = FALSE)
       expect_true(grepl("rm12img", resizer_output_no_images))
-      expect_false(grepl("img", resizer_output_no_images))
+      expect_true(grepl("img", resizer_output_no_images))
 
       # Test custom colors and sizes
       resizer_output_custom <- add.resizer(
@@ -67,6 +62,6 @@ test_that("add.resizer returns an HTML character string with correct class", {
       expect_false(grepl("line.width", resizer_output_custom)) # Should be replaced
       expect_false(grepl("line.height", resizer_output_custom)) # Should be replaced
       expect_true(grepl("'50%'", resizer_output_custom))
-    }
-  )
+
+
 })
